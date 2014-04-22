2a09f8509b6751af5a73c2e5242ea17c843af5fd
ehennenfent <ehennenfent@imsa.edu>
2014-04-22 14:45:27 -0500

    Redoing?
    Not sure if this went through before.

--- Game/Snake.java
+++ Game/Snake.java
@@ -1,19 +1,19 @@
 import java.util.ArrayList;
 
 
-class snake {
+class Snake {
 	int direction = 1;
 	int size = 2;
 	ArrayList<Rectangle> blocks;
 	
-	public void move{
+	public void move(){
 		
 	}
 	public void turn(int newDirection){
 		
 	}
-	public ArrayList getRectangles(){
-		
+	public ArrayList<Rectangle> getRectangles(){
+		return blocks;
 	}
 	public void changeLength(){
 	
