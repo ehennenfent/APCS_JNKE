341bb82980fdcd8596430faf38ca69b2b784a247
ehennenfent <ehennenfent@imsa.edu>
2014-04-22 14:42:43 -0500

    Changes to World and Snake Class
    Unified capitaliziation

--- Game/World.java
+++ Game/World.java
@@ -1,4 +1,45 @@
+import java.util.ArrayList;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 
 public class World {
 
+	private ArrayList<Snake> snakes = new ArrayList<Snake>();
+	private ArrayList<Dot> dots = new ArrayList<Dot>();
+	
+	
+	public World(){
+		
+	}
+	
+	public void start(){
+		try {
+			Display.setDisplayMode(new DisplayMode(600,400));
+		} catch (LWJGLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			Display.create();
+		} catch (LWJGLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
+	public void draw(Rectangle q){
+		
+		
+		
+	}
+	
 }
