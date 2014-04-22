9908fb66f42469eb6286d9a5a8a7ab999c51de9c
BossBrown <jlbgreenbay@gmail.com>
2014-04-22 14:39:27 -0500

    Snake Class
    Skeleton of first snake class rendition

---
+++ Game/Snake.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+
+class snake {
+	int direction = 1;
+	int size = 2;
+	ArrayList<Rectangle> blocks;
+	
+	public void move{
+		
+	}
+	public void turn(int newDirection){
+		
+	}
+	public ArrayList getRectangles(){
+		
+	}
+	public void changeLength(){
+	
+	}
+}
