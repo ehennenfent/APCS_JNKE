a7a68d631806553eeea7dcf7b5c414defb902c9b
BossBrown <jlbgreenbay@gmail.com>
2014-05-22 08:17:25 -0500

    Grades

--- Game/SemesterProject/Grades.java
+++ Game/SemesterProject/Grades.java
@@ -1,4 +1,5 @@
 package SemesterProject;
+
 import java.util.HashMap;
 
 
@@ -7,14 +8,12 @@ public class Grades {
 	private String history;
 	private String science;
 	private String english;
-	private String wellness;
 
-	public Grades(String mG, String hG, String sG, String eG, String wG){
+	public Grades(String mG, String hG, String sG, String eG){
 		math = mG;
 		history = hG;
 		science = sG;
 		english = eG;
-		wellness = wG;
 	}
 	
 	public Grades(){
@@ -22,7 +21,6 @@ public class Grades {
 		history = "80";
 		science = "100";
 		english = "90";
-		wellness = "105";
 	}
 	
 	public String getMath(){
@@ -52,13 +50,6 @@ public class Grades {
 	public void setEnglish(String nE){
 		english = nE;
 	}
-
-	public String getWellness(){
-		return wellness;
-	}
-	public void setWellness(String nW){
-		wellness = nW;
-	}
 	
 	public HashMap<String, String> getGrades(){
 		HashMap<String, String> map = new HashMap<String, String>();
@@ -66,7 +57,6 @@ public class Grades {
 		map.put("History", history);
 		map.put("Science", science);
 		map.put("English", english);
-		map.put("Wellness", wellness);
 		return map;
 	}
 }
